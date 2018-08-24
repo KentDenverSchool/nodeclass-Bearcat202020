47a64698921c21500b7d205917ad39cad3b8c024
Bearcat20 <jude.barakat2@gmail.com>
2018-08-23 22:19:29 -0600

    created node class, added instance data and a constructor for now

--- src/Node.java
+++ src/Node.java
@@ -1,2 +1,18 @@
+import java.util.ArrayList;
+
 public class Node {
+    private int data;
+    private ArrayList<Node> nodes;
+
+
+    public Node (int data, Node node){
+        this.data = data;
+        nodes = new ArrayList<>();
+        nodes.add(node);
+    }
+
+    public void addNode(Node node){
+        nodes.add(node);
+    }
+
 }
