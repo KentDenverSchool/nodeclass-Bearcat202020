5c23b18c74eb752c16d545a49aac964bc4dfd192
Bearcat20 <jude.barakat2@gmail.com>
2018-08-23 22:33:00 -0600

    added the accessors and mutators to node, and I also wrote a NodeDriver class to test out all the features of node

--- src/Node.java
+++ src/Node.java
@@ -1,18 +1,33 @@
 import java.util.ArrayList;
 
 public class Node {
-    private int data;
+    private double data;
     private ArrayList<Node> nodes;
 
 
-    public Node (int data, Node node){
+    public Node (double data, Node node){
         this.data = data;
         nodes = new ArrayList<>();
-        nodes.add(node);
+        if(node != null) {
+            nodes.add(node);
+        }
     }
 
     public void addNode(Node node){
         nodes.add(node);
     }
+
+    public double getData() {
+        return data;
+    }
+
+    public void setData(double data) {
+        this.data = data;
+    }
+
+    public ArrayList<Node> getNodes() {
+        return nodes;
+    }
+
 
 }
---
+++ src/NodeDriver.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class NodeDriver {
+    public static void main(String[] args) {
+
+        Node n1 = new Node(1.23, null);
+        Node n2 = new Node(2.34, n1);
+        Node n3 = new Node(3.45, n1);
+
+        //tests for addNode function
+        n3.addNode(n2);
+
+        //test for getNodes and getData
+        System.out.println("Node 3 is pointing to 2 other nodes (node 1 and node 2)");
+        System.out.println("Node 1's data is " + n3.getNodes().get(0).getData());
+        System.out.println("Node 2's data is " + n3.getNodes().get(1).getData());
+        System.out.println();
+
+        //test for setData
+        System.out.println("what would you like to change Node 1's data to?");
+        n1.setData(new Scanner(System.in).nextDouble());
+        System.out.println("Node 1's data is " + n3.getNodes().get(0).getData());
+        System.out.println("Node 2's data is " + n3.getNodes().get(1).getData());
+    }
+}
